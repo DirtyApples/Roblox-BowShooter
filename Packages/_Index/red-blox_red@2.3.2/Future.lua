d01212a0bb384e63cbe0621f2da35a5faa286a25
DirtyApples <themdirtyapples@gmail.com>
2026-06-05 15:01:31 -0400

    Added Teams Stuff

--- Packages/_Index/red-blox_red@2.3.2/Future.lua
+++ Packages/_Index/red-blox_red@2.3.2/Future.lua
@@ -1,3 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent.Parent["red-blox_future@1.0.1"]["future"])
-export type Future<T...> = REQUIRED_MODULE.Future<T...>
-return REQUIRED_MODULE
+return require(script.Parent.Parent["red-blox_future@1.0.1"]["future"])
--- Packages/_Index/red-blox_red@2.3.2/Guard.lua
+++ Packages/_Index/red-blox_red@2.3.2/Guard.lua
@@ -1,3 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent.Parent["red-blox_guard@1.0.1"]["guard"])
-export type Check<T> = REQUIRED_MODULE.Check<T>
-return REQUIRED_MODULE
+return require(script.Parent.Parent["red-blox_guard@1.0.1"]["guard"])
