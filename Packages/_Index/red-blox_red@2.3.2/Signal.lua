69c534eb06c702cf0cb3210663dcad5911bffea3
DirtyApples <themdirtyapples@gmail.com>
2026-06-07 11:46:30 -0400

    Ui Stuff

--- Packages/_Index/red-blox_red@2.3.2/Signal.lua
+++ Packages/_Index/red-blox_red@2.3.2/Signal.lua
@@ -1,1 +1,3 @@
-return require(script.Parent.Parent["red-blox_signal@2.0.2"]["signal"])
+local REQUIRED_MODULE = require(script.Parent.Parent["red-blox_signal@2.0.2"]["signal"])
+export type Signal<T...> = REQUIRED_MODULE.Signal<T...>
+return REQUIRED_MODULE
